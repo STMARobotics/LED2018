a5a594c6a440194412e95f5172c7cc68e219e333
Rob Barnes <robarnes@microsoft.com>
2018-02-01 15:16:54 -0800

    will it upload now?

--- ledService.py
+++ ledService.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template, request, jsonify, json, url_for
 import datetime
 import time
+import multiprocessing
 from neopixel import *
 import argparse
 import signal
 import sys
+
+POISON_PILL = "STOP"
 
 app = Flask(__name__)
 
@@ -21,8 +24,7 @@ def opt_parse():
 
 # LED strip configuration:
 LED_COUNT      = 37      # Number of LED pixels.
-LED_PIN        = 18      # GPIO pin connected to the pixels (18 uses PWM!).
-#LED_PIN       = 10      # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
+LED_PIN        = 18      # GPIO pin connected to the pixels (18 uses PWM!)dev/spidev0.0).
 LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
 LED_DMA        = 5       # DMA channel to use for generating signal (try 5)
 LED_BRIGHTNESS = 255     # Set to 0 for darkest and 255 for brightest
@@ -31,15 +33,27 @@ LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
 LED_STRIP      = ws.WS2811_STRIP_GRB   # Strip type and colour ordering
 
 # Define functions which animate LEDs in various ways.
+# attempting to allow multiprocess via this post : https://stackoverflow.com/questions/29571671/basic-multiprocessing-with-while-loop
 def colorWipe(strip, color):
-	"""change color of the pixel strip"""
+	# Change color of the pixel strip
+        # Check queue for POISON_PILL, just to delete it
+        if ( in_queue.qsize() > 0):
+                #lets throw that POISON_PILL away
+                ick = in_queue.get()
 	for i in range(strip.numPixels()):
 		strip.setPixelColor(i, color)
 	strip.show()
+        return None
 
-def theaterChase(strip, color, wait_ms=50, iterations=10):
-        """Movie theater light style chaser animation."""
-        for j in range(iterations):
+def theaterChase(strip, color, wait_ms=50):
+        # Movie theater light style chaser animation
+        while True:
+                # Check queue for POISON_PILL
+                if ( in_queue.qsize() > 0):
+                      ick = in_queue.get()
+                      if ick == POISON_PILL:
+                              #time to exit this function
+                              break  
                 for q in range(3):
                         for i in range(0, strip.numPixels(), 3):
                                 strip.setPixelColor(i+q, color)
@@ -47,39 +61,43 @@ def theaterChase(strip, color, wait_ms=50, iterations=10):
                         time.sleep(wait_ms/1000.0)
                         for i in range(0, strip.numPixels(), 3):
                                 strip.setPixelColor(i+q, 0)
-
+        return None 
 
 @app.route('/led', methods=['GET', 'POST'])
 @app.route('/echo', methods = ['GET', 'POST'])
 def ledRequest():
-    if request.method == 'GET':
-        return "OK\nMethod: GET\n"
+        if request.method == 'GET':
+                return "OK\nMethod: GET\n"
 
-    elif request.method == 'POST':
-        content = request.json
-        red = int(content['red'])
-        green = int(content['green'])
-        blue = int(content['blue'])
-        if content['ledFunction'] == "colorWipe":
-           # Create NeoPixel object with appropriate configuration.
-           strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL, LED_STRIP)
-           strip.begin()
-           colorWipe(strip, Color(red, green, blue))  # wipe strip with color
-           print "here in colorwipe"
-
-        elif content['ledFunction'] == "theaterChase":
-           # Create NeoPixel object with appropriate configuration.
-           strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL, LED_STRIP)
-           strip.begin()
-           theaterChase(strip, Color(red, green, blue))  # chase strip with color
-           colorWipe(strip, Color(red, green, blue))  # wipe strip solid 
-           print "here in theaterchase"
-
-
+        elif request.method == 'POST':
+                content = request.json
+                red = int(content['red'])
+                green = int(content['green'])
+                blue = int(content['blue'])
+                # put POISON_PILL in queue to stop any subprocesses
+                in_queue.put(POISON_PILL)
+                if content['ledFunction'] == "colorWipe":
+                        # Create NeoPixel object with appropriate configuration.
+                        strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL, LED_STRIP)
+                        strip.begin()
+                        # this only runs once, so not started as parallel process
+                        colorWipe(strip, Color(red, green, blue))  # wipe strip with color
+                        print "here in colorwipe"
+                elif content['ledFunction'] == "theaterChase":
+                        # Create NeoPixel object with appropriate configuration.
+                        strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL, LED_STRIP)
+                        strip.begin()
+                        # this is run as a parallel process as it will continuously upate
+                        pool.apply_async(theaterChase(strip, Color(red, green, blue)))  # chase strip with color
+                        print "here in theaterchase"
 
         #print content['ledFunction']
         #print content['ledSection']
         return "OK\nMethod: POST\n"
 
 if __name__ == "__main__":
-   app.run(host='0.0.0.0', port=80, debug=True)
+        app.run(host='0.0.0.0', port=80, debug=True)
+        # Configure multiprocessing
+        manager = multiprocessing.Manager()
+        in_queue = manager.Queue()
+        pool = multiprocessing.Pool()
