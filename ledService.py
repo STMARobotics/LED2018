bcea4c38a4e5b7ed048d0fc3e6ca56c9ec56d1ea
Rob Barnes <robarnes@microsoft.com>
2018-02-01 15:05:36 -0800

    added subpressing support - hopefully to run animations in seperate thread

--- ledService.py
+++ ledService.py
@@ -22,7 +22,7 @@ def opt_parse():
 # LED strip configuration:
 LED_COUNT      = 37      # Number of LED pixels.
 LED_PIN        = 18      # GPIO pin connected to the pixels (18 uses PWM!).
-#LED_PIN        = 10      # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
+#LED_PIN       = 10      # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
 LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
 LED_DMA        = 5       # DMA channel to use for generating signal (try 5)
 LED_BRIGHTNESS = 255     # Set to 0 for darkest and 255 for brightest
